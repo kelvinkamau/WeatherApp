3055c04babdc69dd71a63149bc26174de23591d8
Kelvin Kamau <kelvinkamau6808@gmail.com>
2019-07-06 13:52:43 +0300

    Settings + preferences

--- app/src/main/java/app/kelvinkamau/weatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/app/kelvinkamau/weatherapp/ui/settings/SettingsFragment.kt
@@ -1,7 +1,21 @@
 package app.kelvinkamau.weatherapp.ui.settings
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
+import app.kelvinkamau.weatherapp.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Settings"
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = null
+    }
 
 }
