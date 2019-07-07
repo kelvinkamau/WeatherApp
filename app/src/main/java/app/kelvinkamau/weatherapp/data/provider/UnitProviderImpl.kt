b9332a781ddf64a2092abdc1dba762b1003afbed
Kelvin Kamau <kelvinkamau6808@gmail.com>
2019-07-07 20:33:45 +0300

    Location + Preference provider

--- app/src/main/java/app/kelvinkamau/weatherapp/data/provider/UnitProviderImpl.kt
+++ app/src/main/java/app/kelvinkamau/weatherapp/data/provider/UnitProviderImpl.kt
@@ -1,15 +1,9 @@
 package app.kelvinkamau.weatherapp.data.provider
 
 import android.content.Context
-import android.content.SharedPreferences
-import android.preference.PreferenceManager
 import app.kelvinkamau.weatherapp.internal.UnitSystem
 
-class UnitProviderImpl(context: Context) : UnitProvider {
-
-    private val appContext = context.applicationContext
-    private val preferences: SharedPreferences
-        get() = PreferenceManager.getDefaultSharedPreferences(appContext)
+class UnitProviderImpl(context: Context) : PreferenceProvider(context), UnitProvider {
 
     override fun getUnitSystem(): UnitSystem {
         val selectedName = preferences.getString("UNIT_SYSTEM", UnitSystem.METRIC.name)
--- app/src/main/java/app/kelvinkamau/weatherapp/ui/weather/current/CurrentWeatherViewModelFactory.kt
+++ app/src/main/java/app/kelvinkamau/weatherapp/ui/weather/current/CurrentWeatherViewModelFactory.kt
@@ -2,15 +2,17 @@ package app.kelvinkamau.weatherapp.ui.weather.current
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import app.kelvinkamau.weatherapp.data.provider.UnitProvider
 import app.kelvinkamau.weatherapp.data.repository.ForecastRepository
 
 class CurrentWeatherViewModelFactory(
-    private val forecastRepository: ForecastRepository
-):ViewModelProvider.NewInstanceFactory(){
+    private val forecastRepository: ForecastRepository,
+    private val unitProvider: UnitProvider
+) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return CurrentWeatherViewModel(forecastRepository) as T
+        return CurrentWeatherViewModel(forecastRepository, unitProvider) as T
 
     }
 }
